284ad27251e47f7f8c8c8f64df8f0fd6db686d6f
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-07-14 18:03:39 +0300

    Adding Image on main page

--- second_app/lib/main.dart
+++ second_app/lib/main.dart
@@ -38,20 +38,12 @@ class _WidgetDetailState extends State<WidgetDetail> {
           ),),
           leading: const Text("ICON ALANI"),
         ),
-        body:  Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(value.toString(),style: GoogleFonts.acme(
-                fontSize: 40,
-                color: HexColor(primaryColor)
-              )),
-                Text("2024 Flutter ",style: TextStyle(
-                color: HexColor("FFAAAA"),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),)
-          ],),
+        body: const  Center(
+          child: Image(
+            // image: AssetImage("lib/assets/images/img1.jpg"),
+            image: NetworkImage("https://images.pexels.com/photos/25695918/pexels-photo-25695918/free-photo-of-adam-plaj-insanlar-kum.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+          )
+           
         ),
       floatingActionButton: FloatingActionButton(
         child: Text("+"),
